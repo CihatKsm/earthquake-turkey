f2d0fcb2a0e5d5ef8c6ff4c1660b0c7b8c03a5a6
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2023-02-22 21:15:53 +0300

    update

--- earthquake.js
+++ earthquake.js
@@ -4,6 +4,14 @@ let latestData = '';
 
 module.exports.earthquake = new EventEmitter();
 
+setInterval(async () => {
+    const datas = await api()
+    if (latestData.length == 0) latestData = datas[0].date;
+    if (latestData == datas[0].date) return;
+    latestData = datas[0].date
+    module.exports.earthquake.emit('quake', datas[0])
+}, 10 * 1000);
+
 module.exports.earthquakes = async () => await api()
 
 module.exports.earthquakes.get = async ({ minimum, count }) => {
@@ -11,7 +19,7 @@ module.exports.earthquakes.get = async ({ minimum, count }) => {
     if (isNaN(Number(minimum))) return [];
 
     const all = await api()
-    const datas = all.filter(f => Number(f.ml * 10) >= Number(minimum * 10))
+    const datas = all.filter(f => Number(f.ml) >= Number(minimum))
 
     if (datas.length == 0) return [];
     if (count) {
@@ -21,11 +29,3 @@ module.exports.earthquakes.get = async ({ minimum, count }) => {
 
     return datas;
 }
-
-setInterval(async () => {
-    const datas = await api()
-    if (latestData.length == 0) latestData = datas[0].date;
-    if (latestData == datas[0].date) return;
-    latestData = datas[0].date
-    module.exports.earthquake.emit('quake', datas[0])
-}, 10 * 1000);
--- simple.js
+++ simple.js
@@ -6,14 +6,14 @@ function date() {
 }
 console.log(date(), 'System opened!')
 
-quake.earthquake.on('quake', (quake) => {
-    console.log(date(), quake)
+quake.earthquake.on('quake', (info) => {
+    console.log(date(), info)
 })
 
 setTimeout(async () => {
     const all = await quake.earthquakes({ count: 1 })
-    console.log('this is latest quake:', all[0])
+    //console.log('this is latest quake:', all[0])
 
     const min3_8 = await quake.earthquakes.get({ minimum: 3.8, count: 2 })
-    console.log('the latest earthquake of 3.8 and above:', min3_8)
+    //console.log('the latest earthquake of 3.8 and above:', min3_8)
 }, 2000);
