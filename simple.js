8a9527e7415280f5f2c1919efec0f2e1131d2c39
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2023-02-22 15:49:21 +0300

    update

--- simple.js
+++ simple.js
@@ -12,8 +12,8 @@ quake.earthquake.on('quake', (quake) => {
 
 setTimeout(async () => {
     const all = await quake.earthquakes({ count: 1 })
-    //console.log('this is latest quake:', all[0])
+    console.log('this is latest quake:', all[0])
 
     const min3_8 = await quake.earthquakes.get({ minimum: 3.8, count: 2 })
-    //console.log('the latest earthquake of 3.8 and above:', min3_8)
+    console.log('the latest earthquake of 3.8 and above:', min3_8)
 }, 2000);
