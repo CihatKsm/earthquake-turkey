047ad88eadbec9647662878960181263ff24ddbe
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2023-02-22 15:48:05 +0300

    🪄

---
+++ api.js
@@ -0,0 +1,26 @@
+const { default: axios } = require("axios");
+let latestData = []
+
+module.exports = async () => {
+    const url = 'http://www.koeri.boun.edu.tr/scripts/lst1.asp'
+    const api = await axios({ method: 'get', url }).catch((e) => null)
+    if (!api) return latestData;
+
+    const datas = api.data.slice(api.data.indexOf('<pre>')).split("\n").slice(7).slice(0, 500);
+    const array = latestData = datas.map(m => m.replace(`\r`, '')).map(data => {
+        const i = data.split("  ").filter(f => f !== '')
+        return {
+            date: i[0],
+            latitude: i[1],
+            longitude: i[2],
+            depth: i[3],
+            md: i[4],
+            ml: i[5],
+            mw: i[6],
+            place: i[7],
+        }
+    })
+
+    latestData = array;
+    return array;
+};
---
+++ earthquake.js
@@ -0,0 +1,31 @@
+const EventEmitter = require('events');
+const api = require('./api');
+let latestData = '';
+
+module.exports.earthquake = new EventEmitter();
+
+module.exports.earthquakes = async () => await api()
+
+module.exports.earthquakes.get = async ({ minimum, count }) => {
+    if (!minimum) return [];
+    if (isNaN(Number(minimum))) return [];
+
+    const all = await api()
+    const datas = all.filter(f => Number(f.ml * 10) >= Number(minimum * 10))
+
+    if (datas.length == 0) return [];
+    if (count) {
+        if (isNaN(Number(count))) return [];
+        return datas.slice(0, count)
+    }
+
+    return datas;
+}
+
+setInterval(async () => {
+    const datas = await api()
+    if (latestData.length == 0) latestData = datas[0].date;
+    if (latestData == datas[0].date) return;
+    latestData = datas[0].date
+    module.exports.earthquake.emit('quake', datas[0])
+}, 10 * 1000);
